65dc80894aa008c668c095c6a4199926f6fa75fc
Shiva <srkm281955@gmail.com>
2021-02-06 10:23:54 +0200

    added IEmployeeManagementService and EmployeeManagementService

---
+++ EmployeeManagementFrontEnd/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeManagementPortal.MVC.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementPortal.MVC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddRepository(this IServiceCollection services)
+        {
+            services.AddHttpClient<IEmployeeManagementService, EmployeeManagementService>();
+
+
+            return services;
+        }
+    }
+}
---
+++ EmployeeManagementFrontEnd/Services/EmployeeManagementService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementPortal.MVC.Services
+{
+    public class EmployeeManagementService
+    {
+        private readonly HttpClient httpClient;
+        public EmployeeManagementService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+    }
+}
