6ebc7034c111e1465c6367848f4bc99fdfdaf18f
Ahtesham Kamali <v-ahtkam@microsoft.com>
2021-02-06 22:57:15 +0530

    Added Json Parser

--- EmployeeManagement.Infra/Interface/IEmployeeRepository.cs
+++ EmployeeManagement.Infra/Interface/IEmployeeRepository.cs
@@ -1,8 +1,11 @@
 ﻿using EmployeeManagement.Infra.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace EmployeeManagement.Infra.Repositories
 {
     public interface IEmployeeRepository : IGenericRepository<Employee>
     {
+       // async Task<IEnumerable<Employee>> GetAll();
     }
 }
