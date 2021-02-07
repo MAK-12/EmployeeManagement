c6fedcca4616fb693325fc8869644c51b8e91dc9
Ahtesham Kamali <v-ahtkam@microsoft.com>
2021-02-07 14:35:31 +0530

    Added Role

---
+++ EmployeeManagement.Infra/Interface/IRoleRepository.cs
@@ -0,0 +1,11 @@
+﻿using EmployeeManagement.Infra.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Infra.Repositories
+{
+    public interface IRoleRepository : IGenericRepository<Roles>
+    {
+        Task<IEnumerable<Roles>> Find(string searchText);
+    }
+}
